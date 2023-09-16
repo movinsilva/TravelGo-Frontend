d4b8a545238923d5cf8ac5f8c07a7c4cc7835932
movinsilva <movinpinsara@gmail.com>
2023-09-17 01:28:30 +0530

    train-container-responsive

--- src/Screens/Schedule/ScheduleScreen.jsx
+++ src/Screens/Schedule/ScheduleScreen.jsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Schedule from './Schedule'
+import NewSchdule from './NewSchdule';
 
 const ScheduleScreen = () => {
   return (
-    <Schedule />
+    <NewSchdule />
   )
 }
 
